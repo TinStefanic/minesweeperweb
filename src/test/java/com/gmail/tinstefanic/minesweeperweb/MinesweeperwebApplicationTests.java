c9e8478c56bb1dba8437afc259e81db26202a16a
TinStefanic <tin.stefanic@gmail.com>
2022-04-08 16:25:15 +0200

    Project initiated with spring initializr.

---
+++ src/test/java/com/gmail/tinstefanic/minesweeperweb/MinesweeperwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.tinstefanic.minesweeperweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
